00e1d1ade7b7b8d277045d750005aa492e6ef372
Sheikh-Muneeb-Ahmed <183858883+muneebahmed-nust@users.noreply.github.com>
2025-09-09 21:11:48 +0500

    Add hello.cpp starter program

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to DSA Lab 1!" << endl;
+    int x;
+    cin >> x;
+    cout << "You entered: " << x << endl;
+    return 0;
+}
